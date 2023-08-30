396946f4e9f876c642c157918b23daa4da3d804f
Ahmed Raza <bhaia9036@gmail.com>
2023-08-30 13:02:58 +0500

    Preveious Repo Code

--- index.js
+++ index.js
@@ -16,9 +16,12 @@ import fs from 'fs';
 import path from 'path';
 import { tweetModel } from './Models/User.js';
 app.use(cookieParser());
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
-app.use(cors());
+app.use(cors({
+  origin: ['https://www.equipmentsuppliers.co.uk' ,  "*"],
+  credentials: true
+}));
+// app.use(express.json());
 const storage = multer.diskStorage({
   destination: '/tmp',
   filename: function (req, file, cb) {
@@ -28,160 +31,10 @@ const storage = multer.diskStorage({
   }
 });
 const upload = multer({ storage });
-app.post('/login', async (req, res) => {
-  try {
-    let body = req.body;
-    body.email = body.email.toLowerCase();
-
-    if (!body.email || !body.password) {
-      res.status(400).send(`required fields missing, request example: ...`);
-      return;
-    }
-
-    // check if user exists
-    const data = await User.findOne({ email: body.email }, "username email password");
-
-    if (data && body.password === data.password) { // user found
-      console.log("data: ", data);
-
-      const token = jwt.sign({
-        _id: data._id,
-        email: data.email,
-        iat: Math.floor(Date.now() / 1000) - 30,
-        exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
-      }, SECRET);
-
-      console.log("token: ", token);
-
-      res.cookie('Token', token, {
-        maxAge: 86_400_000,
-        httpOnly: true,
-        sameSite: 'none',
-        secure: true
-      });
-
-      res.send({
-        message: "login successful",
-        profile: {
-          email: data.email,
-          firstName: data.firstName,
-          lastName: data.lastName,
-          age: data.age,
-          _id: data._id
-        }
-      });
-
-      return;
-
-
-    }
-    else { // user not found
-      console.log("user not found");
-      res.status(401).send({ message: "Incorrect email or password" });
-    }
-  } catch (error) {
-    console.log("error: ", error);
-    res.status(500).send({ message: "login failed, please try later" });
-  }
-});
-app.post('/signup', async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-
-    // Check if user with the given email already exists
-    const existingUser = await User.findOne({ email });
-
-    if (existingUser) {
-      return res.status(400).json({ error: 'Email already exists' });
-    }
-
-    // Create a new user
-    const newUser = new User({
-      username,
-      email,
-      password,
-    });
-
-    // Save the user to the database
-    await newUser.save();
-
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-app.get('/api/v1/profile', (req, res) => {
-  const _id = req.body.token._id
-  const getData = async () => {
-    try {
-      const user = await User.findOne({ _id: _id }, "email password username -_id").exec()
-      if (!user) {
-        res.status(404).send({})
-        return;
-      } else {
-
-        res.set({
-          "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
-          "Pragma": "no-cache",
-          "Expires": "0",
-          "Surrogate-Control": "no-store"
-        });
-        res.status(200).send(user)
-      }
-
-    } catch (error) {
-
-      console.log("error: ", error);
-      res.status(500).send({
-        message: "something went wrong on server",
-      });
-    }
-
-  }
-  getData()
+app.use(express.json());
+app.get('/' , (req,res)=> {
+  res.send('Ahemd Raza ')
 })
-// app.use('/api/v1', (req, res, next) => {
-
-//   console.log("req.cookies: ", req.cookies.Token);
-
-//   if (!req?.cookies?.Token) {
-//     res.status(401).send({
-//       message: "include http-only credentials with every request"
-//     })
-//     return;
-//   }
-
-//   jwt.verify(req.cookies.Token, SECRET, function (err, decodedData) {
-//     if (!err) {
-
-//       console.log("decodedData: ", decodedData);
-
-//       const nowDate = new Date().getTime() / 1000;
-
-//       if (decodedData.exp < nowDate) {
-
-//         res.status(401);
-//         res.cookie('Token', '', {
-//           maxAge: 1,
-//           httpOnly: true,
-//           sameSite: 'none',
-//           secure: true
-//         });
-//         res.send({ message: "token expired" })
-
-//       } else {
-
-//         console.log("token approved");
-
-//         req.body.token = decodedData
-//         next();
-//       }
-//     } else {
-//       res.status(401).send("invalid token")
-//     }
-//   });
-// })
 app.get('/api/search', async (req, res) => {
   const searchTerm = req.query.q;
   try {
@@ -360,6 +213,161 @@ app.post('/api/v1/AddProduct', upload.any(), (req, res) => {
   } catch (error) {
     console.log("error: ", error);
   }
+})
+app.post('/signup', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // Check if user with the given email already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Email already exists' });
+    }
+
+    // Create a new user
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
+
+    // Save the user to the database
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+app.post('/login', async (req, res) => {
+  try {
+    let body = req.body;
+    body.email = body.email.toLowerCase();
+
+    if (!body.email || !body.password) {
+      res.status(400).send(`required fields missing, request example: ...`);
+      return;
+    }
+
+    // check if user exists
+    const data = await User.findOne({ email: body.email }, "username email password");
+
+    if (data && body.password === data.password) { // user found
+      console.log("data: ", data);
+
+      const token = jwt.sign({
+        _id: data._id,
+        email: data.email,
+        iat: Math.floor(Date.now() / 1000) - 30,
+        exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
+      }, SECRET);
+
+      console.log("token: ", token);
+
+      res.cookie('Token', token, {
+        maxAge: 86_400_000,
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true
+      });
+
+      res.send({
+        message: "login successful",
+        profile: {
+          email: data.email,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          age: data.age,
+          _id: data._id
+        }
+      });
+
+      return;
+
+
+    }
+    else { // user not found
+      console.log("user not found");
+      res.status(401).send({ message: "Incorrect email or password" });
+    }
+  } catch (error) {
+    console.log("error: ", error);
+    res.status(500).send({ message: "login failed, please try later" });
+  }
+});
+
+app.use('/api/v1', (req, res, next) => {
+
+  console.log("req.cookies: ", req.cookies.Token);
+
+  if (!req?.cookies?.Token) {
+    res.status(401).send({
+      message: "include http-only credentials with every request"
+    })
+    return;
+  }
+
+  jwt.verify(req.cookies.Token, SECRET, function (err, decodedData) {
+    if (!err) {
+
+      console.log("decodedData: ", decodedData);
+
+      const nowDate = new Date().getTime() / 1000;
+
+      if (decodedData.exp < nowDate) {
+
+        res.status(401);
+        res.cookie('Token', '', {
+          maxAge: 1,
+          httpOnly: true,
+          sameSite: 'none',
+          secure: true
+        });
+        res.send({ message: "token expired" })
+
+      } else {
+
+        console.log("token approved");
+
+        req.body.token = decodedData
+        next();
+      }
+    } else {
+      res.status(401).send("invalid token")
+    }
+  });
+})
+app.get('/api/v1/profile', (req, res) => {
+  const _id = req.body.token._id
+  const getData = async () => {
+    try {
+      const user = await User.findOne({ _id: _id }, "email password username -_id").exec()
+      if (!user) {
+        res.status(404).send({})
+        return;
+      } else {
+
+        res.set({
+          "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+          "Pragma": "no-cache",
+          "Expires": "0",
+          "Surrogate-Control": "no-store"
+        });
+        res.status(200).send(user)
+      }
+
+    } catch (error) {
+
+      console.log("error: ", error);
+      res.status(500).send({
+        message: "something went wrong on server",
+      });
+    }
+
+  }
+  getData()
 })
 app.post('/logout', (req, res) => {
   try {
