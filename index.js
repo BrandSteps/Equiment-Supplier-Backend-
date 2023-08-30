f8bdbea0e8b58199e955f15ba63b965e5df6cc37
Ahmed Raza <bhaia9036@gmail.com>
2023-08-30 12:34:37 +0500

    Little Changes

--- index.js
+++ index.js
@@ -31,6 +31,89 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 app.use(express.json());
+app.post('/signup', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // Check if user with the given email already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Email already exists' });
+    }
+
+    // Create a new user
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
+
+    // Save the user to the database
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+app.post('/login', async (req, res) => {
+  try {
+    let body = req.body;
+    body.email = body.email.toLowerCase();
+
+    if (!body.email || !body.password) {
+      res.status(400).send(`required fields missing, request example: ...`);
+      return;
+    }
+
+    // check if user exists
+    const data = await User.findOne({ email: body.email }, "username email password");
+
+    if (data && body.password === data.password) { // user found
+      console.log("data: ", data);
+
+      const token = jwt.sign({
+        _id: data._id,
+        email: data.email,
+        iat: Math.floor(Date.now() / 1000) - 30,
+        exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
+      }, SECRET);
+
+      console.log("token: ", token);
+
+      res.cookie('Token', token, {
+        maxAge: 86_400_000,
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true
+      });
+
+      res.send({
+        message: "login successful",
+        profile: {
+          email: data.email,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          age: data.age,
+          _id: data._id
+        }
+      });
+
+      return;
+
+
+    }
+    else { // user not found
+      console.log("user not found");
+      res.status(401).send({ message: "Incorrect email or password" });
+    }
+  } catch (error) {
+    console.log("error: ", error);
+    res.status(500).send({ message: "login failed, please try later" });
+  }
+});
 app.get('/api/v1/profile', (req, res) => {
   const _id = req.body.token._id
   const getData = async () => {
@@ -281,89 +364,6 @@ app.post('/api/v1/AddProduct', upload.any(), (req, res) => {
     console.log("error: ", error);
   }
 })
-app.post('/signup', async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-
-    // Check if user with the given email already exists
-    const existingUser = await User.findOne({ email });
-
-    if (existingUser) {
-      return res.status(400).json({ error: 'Email already exists' });
-    }
-
-    // Create a new user
-    const newUser = new User({
-      username,
-      email,
-      password,
-    });
-
-    // Save the user to the database
-    await newUser.save();
-
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-app.post('/login', async (req, res) => {
-  try {
-    let body = req.body;
-    body.email = body.email.toLowerCase();
-
-    if (!body.email || !body.password) {
-      res.status(400).send(`required fields missing, request example: ...`);
-      return;
-    }
-
-    // check if user exists
-    const data = await User.findOne({ email: body.email }, "username email password");
-
-    if (data && body.password === data.password) { // user found
-      console.log("data: ", data);
-
-      const token = jwt.sign({
-        _id: data._id,
-        email: data.email,
-        iat: Math.floor(Date.now() / 1000) - 30,
-        exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
-      }, SECRET);
-
-      console.log("token: ", token);
-
-      res.cookie('Token', token, {
-        maxAge: 86_400_000,
-        httpOnly: true,
-        sameSite: 'none',
-        secure: true
-      });
-
-      res.send({
-        message: "login successful",
-        profile: {
-          email: data.email,
-          firstName: data.firstName,
-          lastName: data.lastName,
-          age: data.age,
-          _id: data._id
-        }
-      });
-
-      return;
-
-
-    }
-    else { // user not found
-      console.log("user not found");
-      res.status(401).send({ message: "Incorrect email or password" });
-    }
-  } catch (error) {
-    console.log("error: ", error);
-    res.status(500).send({ message: "login failed, please try later" });
-  }
-});
 app.post('/logout', (req, res) => {
   try {
     // Clear the token from the client-side by setting an expired cookie
